431f93390ee241c5a8bbb3f8378e505b7d90f843
rohindh2011 <144439338+rohindh2011@users.noreply.github.com>
2023-09-08 15:52:25 +0530

    Revert "rohindh"

--- EX1.py
+++ EX1.py
@@ -1,12 +1,6 @@
 print("jeevitha Python file")
-
-print("Rohindh Kishore K M")
-
-
-
 print("Jila")
 print("Sanskriti")
-
 print("Suraj Karthic Python file")
 print("Rohindh")
 print("Aditya Trivedi")
